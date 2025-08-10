a8e4b27e097f14bcac845cba24fa92b623d341f4
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-10 23:48:11 +0530

    Add middleware for JSON and URL-encoded body parsing in main.js

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
     // res.send('Hello, Express!');
 
@@ -32,6 +38,21 @@ app.get('/error', (req, res) => {
         message: 'Something went wrong!',
         timestamp: new Date().toISOString(),
         status: 'error',
+        environment: process.env.NODE_ENV || 'development'
+    });
+});
+
+app.post('/data', (req, res) => {
+
+    const { email, username, mobile } = req.body;
+    console.log('Received user data:', { email, username, mobile });
+
+
+    // Simulate data processing
+    res.json({
+        message: 'Data received successfully',
+        status: 'success',
+        timestamp: new Date().toISOString(),
         environment: process.env.NODE_ENV || 'development'
     });
 });
